d3b4ff27998b7125e17a2c4774783f92665d4014
daguenette <da.guenette@icloud.com>
2024-02-18 14:10:23 -0500

    Fixed flame

--- scripts/flame_projectile.gd
+++ scripts/flame_projectile.gd
@@ -3,12 +3,21 @@ extends Area3D
 var projectil_flame_scene = preload("res://arts/vfx/vfx_Flametrower.tscn")
 @onready var anchor: Node3D = $Anchor
 
+func _ready() -> void:
+	%Timer.start()
+
 func set_properties():
 	var projectile_instance = projectil_flame_scene.instantiate()
 	anchor.add_child(projectile_instance)
-	print("Flame added")
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("plane"):
 		body._hit_by_projectile()
 		queue_free()
+
+
+func _on_timer_timeout() -> void:
+	var fire_effects = get_tree().get_first_node_in_group("fire_effect")
+	for fire_effect in fire_effects.get_children():
+		queue_free()
+	
