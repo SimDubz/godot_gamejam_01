8eaf13add080ac1dcd66dd3ddf8bf9b3088022b9
daguenette <da.guenette@icloud.com>
2024-02-18 02:58:35 -0500

    Fixed rotation

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -23,17 +23,21 @@ func _on_body_entered(body: Node3D) -> void:
 ### UTILS FUNCTIONS ###
 
 func set_properties(new_direction: Vector3, projectile_type: GameData.WEAPON) -> void:
-	direction = new_direction
+	direction = new_direction.normalized()
 	
+	var projectile_instance
 	if projectile_type == GameData.WEAPON.CANON:
-		var projectile_canon = projectil_canon_scene.instantiate()
-		anchor.add_child(projectile_canon)
-		collision_shape_3d.scale = projectile_canon.scale
+		projectile_instance = projectil_canon_scene.instantiate()
+	elif projectile_type == GameData.WEAPON.ARROW:
+		projectile_instance = projectil_arrow_scene.instantiate()
+	
+	if projectile_instance:
+		anchor.add_child(projectile_instance)
+		collision_shape_3d.scale = projectile_instance.scale
+		var target_look_at = projectile_instance.global_transform.origin - direction
+		projectile_instance.look_at(target_look_at, Vector3.UP)
 		
-	if projectile_type == GameData.WEAPON.ARROW:
-		var projectile_arrow = projectil_arrow_scene.instantiate()
-		anchor.add_child(projectile_arrow)
-		collision_shape_3d.scale = projectile_arrow.scale
+		# If additional setup is needed, do it here
 
 func _on_timer_timeout() -> void:
 	queue_free()
