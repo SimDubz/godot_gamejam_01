3666e9012aba59952f09d3c06581c9cfaf3850c2
daguenette <da.guenette@icloud.com>
2024-02-18 21:18:20 -0500

    Fix fire

--- scripts/flame_projectile.gd
+++ scripts/flame_projectile.gd
@@ -5,14 +5,6 @@ var projectil_flame_scene = preload("res://arts/vfx/vfx_Flametrower.tscn")
 
 
 func _on_body_entered(body: Node3D) -> void:
-	print("Body entered")
 	if body.is_in_group("plane"):
 		body._hit_by_projectile()
-		queue_free()
 
-
-func _on_timer_timeout() -> void:
-	var fire_effects = get_tree().get_first_node_in_group("fire_effect")
-	for fire_effect in fire_effects.get_children():
-		queue_free()
-	
